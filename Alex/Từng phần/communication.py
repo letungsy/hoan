d52140d6a78a5c7424233510b854b9a1f16b0d3e
letungsy <letungsy3005@gmail.com>
2024-06-23 14:39:21 +0700

    hoan pro

---
+++ Alex/Từng phần/communication.py
@@ -0,0 +1,11 @@
+from time import strftime
+
+
+def hello(name):
+    day_time = int(strftime('%H'))
+    if day_time < 12:
+        speak("Chào buổi sáng bạn {}. Chúc bạn một ngày tốt lành..".format(name))
+    elif 12 <=day_time < 18:
+        speak("Chào buổi chiều bạn {}. Bạn đã dự định gì cho chiều nay chưa.".format(name))
+    else:
+        speak("Chào buổi tối bạn {}. Bạn đã ăn tối chưa nhỉ.".format(name))

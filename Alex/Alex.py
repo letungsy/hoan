9b83828b1533ecde6c2736d9ccf2a8deeff1407e
letungsy <69799200+letungsy@users.noreply.github.com>
2024-06-23 14:55:53 +0700

    Update Alex.py

--- Alex/Alex.py
+++ Alex/Alex.py
@@ -33,19 +33,19 @@ def speak(text):
 
 
 # Speech - to - text: Chuyển đổi giọng nói bạn yêu cầu vào thành văn bản hiện ra khi máy trả lại kết quả đã nghe
-def get_audio():
-    print("\nBot: \tĐang nghe \t --__-- \n")
-    # r = sr.Recognizer()
-    # with sr.Microphone() as source:
-        print("Tôi: ", end='')
-        audio = r.listen(source, phrase_time_limit=8)
-        try:
-            text = r.recognize_google(audio, language="vi-VN")
-            print(text)
-            return text.lower()
-        except:
-            print("...")
-            return 0
+# def get_audio():
+#     print("\nBot: \tĐang nghe \t --__-- \n")
+#     # r = sr.Recognizer()
+#     # with sr.Microphone() as source:
+#         print("Tôi: ", end='')
+#         audio = r.listen(source, phrase_time_limit=8)
+#         try:
+#             text = r.recognize_google(audio, language="vi-VN")
+#             print(text)
+#             return text.lower()
+#         except:
+#             print("...")
+#             return 0
 
 
 # Ở dòng này, Bot sẽ chào tạm biệt bạn khi bạn tạm biệt nó ^^ 

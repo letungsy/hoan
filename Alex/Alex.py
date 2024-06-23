f858b4f2db6f07aec02e75f58e5c03c5707f201f
letungsy <69799200+letungsy@users.noreply.github.com>
2024-06-23 14:59:05 +0700

    Update Alex.py

--- Alex/Alex.py
+++ Alex/Alex.py
@@ -14,7 +14,7 @@ import urllib.request as urllib2
 # from selenium.webdriver.common.keys import Keys
 # from webdriver_manager.chrome import ChromeDriverManager
 from time import strftime
-from gtts import gTTS
+# from gtts import gTTS
 from youtube_search import YoutubeSearch
 
 # Khúc này là khai báo các biến cho quá trình làm con Alex
